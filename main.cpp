1d93fa872ba9a527090db56d8ffe6a2dab85556f
Dennis <denchyk228@gmail.com>
2023-05-06 14:07:42 +0200

    added the parsing of command line arguments

--- main.cpp
+++ main.cpp
@@ -3,18 +3,34 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    // ./main examples_2/var1
+
+    if (argc != 2)
+    {
+        cerr << "main: program requires 1 positional argument - directory path\n";
+        cerr << "usage: main <directory_path>\n";
+        return 1;
+    }
+
+    string directory = argv[1];
+
     try
     {
         CountryCSVReader countries_reader;
-        CountriesTable table = countries_reader.readAndParseCountriesFromDirectory("examples_2/var1");
+        CountriesTable table = countries_reader.readAndParseCountriesFromDirectory(directory);
         table.calculate_winners();
         table.create_winners_csv();
+        cout << "File results.csv was successfully created.\n";
+    }
+    catch (const invalid_argument &e)
+    {
+        cerr << e.what() << '\n';
     }
     catch (const exception &e)
     {
-        cerr << e.what() << '\n';
+        cerr << "The problem during the program execution: " << e.what() << '\n';
     }
 
     return 0;
